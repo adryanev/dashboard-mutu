8b3fba3aa2e3a02bf8e9e2454574d78d1ad79bf6
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-08-05 11:07:13 +0700

    test new deployment

--- admin/views/site/index.php
+++ admin/views/site/index.php
@@ -19,9 +19,9 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="kt-portlet">
             <div class="kt-portlet__head">
                 <div class="kt-portlet__head-label">
-												<span class="kt-portlet__head-icon">
-													<i class="flaticon2-dashboard"></i>
-												</span>
+                       <span class="kt-portlet__head-icon">
+                              <i class="flaticon2-dashboard"></i>
+                                               </span>
                     <h3 class="kt-portlet__head-title">
                         Selamat Datang
                         <small>di Dashboard Admin</small>
@@ -44,6 +44,3 @@ $this->params['breadcrumbs'][] = $this->title;
         <!--end::Portlet-->
     </div>
 </div>
-
-
-
