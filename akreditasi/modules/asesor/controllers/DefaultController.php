0df4918cd76ee7381ea4978b280dda59921e952e
Adryan Eka Vandra <adryanekavandra@gmail.com>
2021-10-18 20:40:26 +0700

    perbaiki controller asesor

--- akreditasi/modules/asesor/controllers/DefaultController.php
+++ akreditasi/modules/asesor/controllers/DefaultController.php
@@ -24,9 +24,8 @@ class DefaultController extends BaseController
     public function actionIndex()
     {
         $apsDataProvider = new ActiveDataProvider(['query' => K9AkreditasiProdi::find()]);
-        $aptDataProvider = new ActiveDataProvider(['query' => K9AkreditasiInstitusi::find()]);
 
-        return $this->render('index', compact('apsDataProvider', 'aptDataProvider'));
+        return $this->render('index', compact('apsDataProvider'));
     }
 
     /**
@@ -47,21 +46,4 @@ class DefaultController extends BaseController
 
     }
 
-    /**
-     * @param $id
-     * @return \yii\console\Response|\yii\web\Response
-     * @throws NotFoundHttpException
-     */
-    public function actionKuantitatifInstitusi($id)
-    {
-        $kuantitatif = K9DataKuantitatifInstitusi::findOne(['id_akreditasi_institusi' => $id]);
-        if (!$kuantitatif) {
-            throw new NotFoundHttpException();
-        }
-
-        $path = K9InstitusiDirectoryHelper::getKuantitatifPath($kuantitatif->akreditasiInstitusi);
-        return Yii::$app->response->sendFile("$path/{$kuantitatif->isi_dokumen}");
-
-
-    }
 }
