8b9ffaec40df556fe3d987288c885429f939272e
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-03-12 20:20:33 +0700

    delete name from footer

--- akreditasi/views/layouts/footer.php
+++ akreditasi/views/layouts/footer.php
@@ -7,7 +7,7 @@ use yii\bootstrap4\Html;
 <div class="kt-footer kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
     <div class="kt-footer__copyright">
         <?= date('Y') ?>&nbsp;&copy;&nbsp;<a href="https://github.com/adryanev" target="_blank" class="kt-link">
-            <?= Html::encode(Yii::$app->params['institusi']) ?> by Adryan Eka Vandra</a>
+            <?= Html::encode(Yii::$app->params['institusi']) ?>  </a>
     </div>
     <div class="kt-footer__menu">
         <a href="<?= Yii::$app->params['url_institusi'] ?>" target="_blank"
